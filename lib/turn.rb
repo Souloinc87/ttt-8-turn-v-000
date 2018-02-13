8f26b83e7cdafa96ed8aa0ba66888ab87b62d7cf
Souloinc87 <souloinc87@gmail.com>
2018-02-13 21:20:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,7 @@ def turn(board)
   user_input = gets
   index = input_to_index(user_input)
   valid_move?(board, index)
-  valib_move?(board, invalid)
+  valid_move?(board, invalid)
   move(board, index, value = "X")
   display_board(board)
 

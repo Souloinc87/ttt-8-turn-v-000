07189ecd747bafa492c654bf8f5e57db727b2f1d
Souloinc87 <souloinc87@gmail.com>
2018-02-13 17:18:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,15 +9,14 @@ end
 def valid_move?(board, index)
 	if position_taken?(board, index)
 		board{index] == false
-        else 
+        else
 		index.between?(0, 8)
 	end
  end
 
- def position_taken?(board, index) 
+ def position_taken?(board, index)
 	(board[index] == “  “ | | board{index] == “” | | board[index] == nil) == false
-end 
+end
 
 def move(board, user_input, default = X)
   
-

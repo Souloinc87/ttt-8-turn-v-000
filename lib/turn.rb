76941ececc51c995480afa2c8ddbd173c9e4d885
Souloinc87 <souloinc87@gmail.com>
2018-02-13 17:04:29 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,16 +2,3 @@ def turn(board)
   puts "Please enter 1-9:"
 end
 
-def display_board(board)
-  puts " X | X | X "
-  puts "-----------"
-  puts " X | O | O "
-  puts "-----------"
-  puts " X | O | O "
-
-  puts " X | O | X "
-  puts "-----------"
-  puts " O | X | X "
-  puts "-----------"
-  puts " O | X | O "
-end

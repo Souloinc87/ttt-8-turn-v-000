f9c93c54eaf1419999e70667d4fd72e1ea806707
Souloinc87 <souloinc87@gmail.com>
2018-02-13 16:49:44 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,4 +4,10 @@ def display_board(board)
   puts " X | O | O "
   puts "-----------"
   puts " X | O | O "
+
+  puts " X | O | X "
+  puts "-----------"
+  puts " O | X | X "
+  puts "-----------"
+  puts " O | X | O "
 end

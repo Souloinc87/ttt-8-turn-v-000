351c6688424850483e1cde83d7c6f63351394f4b
Souloinc87 <souloinc87@gmail.com>
2018-02-13 17:15:39 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,23 @@
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def valid_move?(board, index)
+	if position_taken?(board, index)
+		board{index] == false
+        else 
+		index.between?(0, 8)
+	end
+ end
+
+ def position_taken?(board, index) 
+	(board[index] == “  “ | | board{index] == “” | | board[index] == nil) == false
+end 
+
+def move(board, user_input, default = X)
+  
+

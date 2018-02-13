6138b23064084ff285b614ef3c2869cf16cb1ec7
Souloinc87 <souloinc87@gmail.com>
2018-02-13 17:49:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,3 +27,11 @@ end
 def position_taken?(board, index)
   (board[index] == " " || board[index] == "" || board[index] == nil) == false
 end
+
+def move(board, index, "O")
+  board[0] = "X"
+  board[8] = "O"
+end 
+
+
+  

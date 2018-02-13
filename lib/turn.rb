8a878ee8f621b66a41d80fdca15a528e091947e5
Souloinc87 <souloinc87@gmail.com>
2018-02-13 19:22:27 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,11 +24,10 @@ def position_taken?(board, index)
 end
 
 def move(board, index, value = "X")
-  board[index] = value 
+  board[index] = value
 end
 
 def turn(board)
   puts "Please enter 1-9:"
   turn = gets.user_input
 end
-
